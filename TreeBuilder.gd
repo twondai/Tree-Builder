453d71d8086729b50561e6bd8683f342d596664b
twondai <tseacow@gmail.com>
2024-09-06 11:09:39 +0100

    More Meshing

--- TreeBuilder.gd
+++ TreeBuilder.gd
@@ -1,7 +1,7 @@
 extends MeshInstance2D
 
 #todo
-#switch to mesh-based tree
+#use nodes to help control branches and keep track of vertices and indices
 #swaying/wind
 
 @export_category("Branch Count")
@@ -35,6 +35,7 @@ extends MeshInstance2D
 	set(v):
 		length_curve = v
 
+var debug_dot = preload("res://debug_dot.tscn")
 
 var rng = RandomNumberGenerator.new()
 @export var random_seed = 420:
@@ -52,12 +53,18 @@ var v_id_array:PackedInt32Array
 
 var node_ready = false
 
+func place_dot(pos:Vector2):
+	var dot = debug_dot.instantiate()
+	dot.position = pos
+	add_child(dot)
+	
+
 func root_branch():
 	var v:PackedVector2Array
 	var color:Color = color_gradient.sample(0)
 	var length:float = length_curve.sample(0)
 	var bottom_width:float = width_curve.sample(0) / 2.
-	var top_width:float = width_curve.sample(1) / 2.
+	var top_width:float = width_curve.sample(1. / float(iterations)) / 2.
 	v.append_array([Vector2(-bottom_width,0),
 	Vector2(-top_width,-length),
 	Vector2(top_width,-length),
@@ -68,9 +75,9 @@ func root_branch():
 	colors.append_array([color,color,color,color])
 
 
-func new_branch(v1:Vector2,v4:Vector2,v_id:int,local_angle:float) -> PackedVector2Array:
-	var length:float = length_curve.sample(v_id)
-	var width = width_curve.sample(v_id) / 2.
+func new_branch(v1:Vector2,v4:Vector2,height:float,local_angle:float) -> PackedVector2Array:
+	var length:float = length_curve.sample(height)
+	var width = width_curve.sample(height) / 2.
 	
 	var local_right = (v4 - v1).angle() + local_angle
 	var local_up = local_right - (PI/2.)
@@ -87,16 +94,39 @@ func new_branch(v1:Vector2,v4:Vector2,v_id:int,local_angle:float) -> PackedVecto
 func branch(i1:int,i2:int,v_id:int = 1):
 	var start_point:Vector2 = vertices[i1]
 	var end_point:Vector2 = vertices[i2]
+	var bottom_points:PackedVector2Array = [start_point]
+	var bottom_indices:PackedInt32Array = [i1]
 	var last_vertex:int = vertices.size() - 1
 	var height = float(v_id) / float(iterations)
 	var color:Color = color_gradient.sample(height)
+	
+	#create bottom points
+	var center = (start_point + end_point) / 2.
+	var radius = (end_point - start_point).length() / 2.
+	var angle_incr = PI / float(branches)
+	var local_right = (end_point - start_point).angle()
+	
+	for i in branches - 1:
+		var a = local_right + ((i+1) * angle_incr)
+		var p:Vector2 = center + Vector2(-radius, 0).rotated(a)
+		a -= angle_incr / 2.
+		var p2:Vector2 = center + Vector2(-radius, 0).rotated(a)
+		place_dot(p2)
+		vertices.append(p)
+		last_vertex = vertices.size() - 1
+		bottom_indices.append(last_vertex)
+		colors.append(color)
+		v_id_array.append(v_id)
+	bottom_indices.push_back(i2)
 	for b in branches:
 		var local_angle = lerp(-angle,angle, float(b) / float(branches - 1))
-		var branch:PackedVector2Array = new_branch(start_point,end_point,v_id,local_angle)
+		var branch:PackedVector2Array = new_branch(start_point,end_point,height,local_angle)
 		vertices.append_array(branch)
 		last_vertex = vertices.size() - 1
-		indices.append_array([i1,last_vertex - 1, last_vertex])
-		indices.append_array([i1,last_vertex, i2])
+		indices.append_array([bottom_indices[b],last_vertex - 1, last_vertex])
+		indices.append_array([bottom_indices[b],last_vertex, bottom_indices[b+1]])
+		v_id_array.append_array([v_id, v_id + 1, v_id + 1])
+		v_id_array.append_array([v_id, v_id + 1, v_id])
 		colors.append_array([color,color])
 		if v_id < iterations:
 			branch(last_vertex - 1,last_vertex, v_id + 1)
@@ -111,7 +141,7 @@ func _ready() -> void:
 	arrays[Mesh.ARRAY_VERTEX] = vertices
 	arrays[Mesh.ARRAY_INDEX] = indices
 	arrays[Mesh.ARRAY_COLOR] =  colors
-	var flags = Mesh.ARRAY_FORMAT_VERTEX + Mesh.ARRAY_FORMAT_COLOR + Mesh.ARRAY_FORMAT_INDEX
+	var flags = Mesh.ARRAY_FORMAT_VERTEX + Mesh.ARRAY_FORMAT_COLOR + Mesh.ARRAY_FORMAT_INDEX 
 	tree_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,arrays,[],{},flags)
 	mesh = tree_mesh
 
